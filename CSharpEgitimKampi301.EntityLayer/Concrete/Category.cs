348748cf67335825a36d01fd2db57fd057162604
Sinan Dogan <sinandogan360@gmail.com>
2025-08-13 13:44:45 +0300

    Layers are created

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        //class oluşturup table a dönüştürme yapıcaz propertyler column olacak
+        // access modifiers public private ınternal protected olur
+        //ınternal sadece bulunduğu katmandan erişim
+        // field variable property class içine tanımlarsak field method içi variable
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+
+
+
+
+    }
+}
